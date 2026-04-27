63d887d4c1d12587f5c7664b9f1efbeba56377ab
Alex Blackbrow <blackbrow@mail.ru>
2026-04-27 20:33:30 +0300

    fix: Исправил недочеты

--- index.php
+++ index.php
@@ -1,24 +1,20 @@
 <?php
-    $variable = 3.14;
-    // $variable = 3;
-    // $variable = 'one';
-    // $variable = true;
-    // $variable = null;
-    // $variable = [];
+$variable = 3.14;
 
-    if (is_bool($variable)) {
-        $type = 'bool';
-    } elseif (is_int($variable)) {
-        $type = 'int';
-    } elseif (is_float($variable)) {
-        $type = 'float';
-    } elseif (is_string($variable)) {
-        $type = 'string';
-    } elseif (is_null($variable)) {
-        $type = 'null';
-    } else {
-        $type = 'other';
-    }
+if (is_bool($variable)) {
+    $type = 'bool';
+} elseif (is_int($variable)) {
+    $type = 'int';
+} elseif (is_float($variable)) {
+    $type = 'float';
+} elseif (is_string($variable)) {
+    $type = 'string';
+} elseif (is_null($variable)) {
+    $type = 'null';
+} else {
+    $type = 'other';
+}
 
-    echo "type is $type";
+echo "type is $type";
+
 ?>
